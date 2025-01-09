e99ec202ffcb1fce4f668b09d3f856081b70d74c
av1nDs0uza <avinalricdsouza@gmail.com>
2025-01-09 10:36:26 +0530

    arrays

---
+++ Array/index.js
@@ -0,0 +1,5 @@
+// HOw do you create an empty array in javascript?
+
+const arr = [1,2,3,4, "Hello", {name:"Avin"}, [1,2,3]];
+
+console.log(arr);
--- countDigit.js
+++ countDigit.js
@@ -1,19 +1,16 @@
-// Q2: Sum of digits of a number
+// Q3: Count the number of digits
 
-//1234 = 1+2+3+4
 
-// 1234%10, remainder = 4, Q =  
-
-function sumOfDigit(num){
+function countDigit(num){
+    num = Math.abs(num);
     let sum = 0;
     // Loop through the number until it becomes 0
     while (num > 0) {
         let rem = num % 10;
-        sum += rem;
+        sum += 1;
         num = Math.floor(num / 10);
     }
-    
     return sum;
 }
 
-console.log(sumOfDigit(123456789))
+console.log(countDigit(123456789))
